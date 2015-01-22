8ff413296e34a9c9378c3e56a1f1b2c58507d6a9
Deepika Tiwari <deepikatiwari92@gmail.com>
2015-01-22 18:50:24 +0530

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+User.create!(name: 'Deepika Tiwari',
+             email: 'deepikatiwari92@example.com')
+
+Micropost.delete_all
+Micropost.create!(content: 'This is the first micropost.',
+                  user_id: 1)
+
+Micropost.create!(content: 'This is the second micropost.',
+                  user_id: 1)
